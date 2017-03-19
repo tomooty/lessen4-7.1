4997921494347bb1896c56836d9fed0293d89898
tomoko-tajiri <tajiri@omiya-lc.com>
2017-03-19 17:17:33 +0900

    lessen4 7.1

--- test3.5/ViewController.swift
+++ test3.5/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5 // 5個のデータがあるという意味
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // 再利用可能な cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        
+        // 値を設定する.
+        cell.textLabel!.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
 }
+
+
+
+
 
